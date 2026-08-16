d242ad4d91c02ec54a8e87bf5c1f853e48f450ae
Sebastian <manredfan10@gmail.com>
2026-08-16 19:20:34 -0400

    aaa

---
+++ app/src/app/test-supabase/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>AppPsi</h1>
+      <p>Aplicación funcionando correctamente.</p>
+    </main>
+  );
+}
---
+++ app/src/app/test-supabase/test-supabase.tsx
@@ -0,0 +1,9 @@
+
+export default function TestSupabase() {
+  return (
+    <main>
+      <h1>Test Supabase</h1>
+      <p>La ruta funciona correctamente.</p>
+    </main>
+  );
+}
